b04fc63fd24f026cc2740a287e9049a0c89069e0
Sandeep Tengali <stengali33@gmail.com>
2017-10-03 23:47:13 -0700

    Update PascalsTriangle.java

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -11,7 +11,7 @@ public class PascalsTriangle {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         ArrayList<Integer> row = new ArrayList<Integer>();
         for(int i=0;i<numRows;i++){
-            row.add(0,1);
+            row.add(0,1); // This will append 1 to the list
             for(int j=1;j<row.size()-1;j++){
                 row.set(j,row.get(j) + row.get(j+1));
             }
