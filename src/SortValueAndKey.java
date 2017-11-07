d56c4135cc7005d3c1ad2947cfa4d64b34319577
stengali <stengali@paypal.com>
2017-11-06 19:17:42 -0800

    Update

---
+++ src/SortValueAndKey.java
@@ -0,0 +1,30 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by stengali on 10/15/17.
+ */
+public class SortValueAndKey {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+    }
+}
+/*
+
+class AgeComparator implements Comparator {
+    public int compare(Object o1,Object o2){
+
+        if(o1.getValue()==o2.getValue()){
+            if(o1.key >= o2.key)
+                return 1;
+            return -1;
+        }
+        else if(o1.getValue()>o2.getValue())
+            return 1;
+        else
+            return -1;
+    }
+}
+*/
