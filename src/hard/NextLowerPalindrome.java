5b34f1be4d1a28ca2d2076e85d7e4058f7ed6d8d
stengali <stengali@paypal.com>
2018-01-19 12:59:38 -0800

    NextLowerPalindrome

---
+++ src/hard/NextLowerPalindrome.java
@@ -0,0 +1,12 @@
+package hard;
+
+/**
+ * Created by stengali on 1/19/18.
+ */
+public class NextLowerPalindrome {
+
+    public static void main(String[] args) {
+        NextLowerPalindrome obj = new NextLowerPalindrome();
+        System.out.println();
+    }
+}
