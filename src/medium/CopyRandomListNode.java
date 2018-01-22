5e41a7421d8ae33047f909a68fd918d7b829efb8
stengali <stengali@paypal.com>
2018-01-21 21:07:24 -0800

    Copy list with Random pointer

---
+++ src/medium/CopyRandomListNode.java
@@ -0,0 +1,42 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by stengali on 1/21/18.
+ */
+
+class RandomListNode {
+    int label;
+    RandomListNode next, random;
+    RandomListNode(int x) { this.label = x; }
+}
+
+public class CopyRandomListNode {
+
+    public RandomListNode copyRandomListPointer(RandomListNode head) {
+        if(head == null)
+            return null;
+
+        RandomListNode node = head;
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+
+        for (node != null) {
+            map.put(node, new RandomListNode(node.label));
+            node = node.next;
+        }
+
+        node = head;
+        while (node != head) {
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+        return map.get(head);
+    }
+    public static void main(String[] args) {
+        CopyRandomListNode obj = new CopyRandomListNode();
+        System.out.println();
+    }
+}
