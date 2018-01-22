0d391d0a55ec5e5b11c66c0e0f5f16aa12bd6484
stengali <stengali@paypal.com>
2018-01-21 22:20:01 -0800

    NestedInteger

---
+++ src/medium/NestedInteger.java
@@ -0,0 +1,12 @@
+package medium;
+
+import java.util.List;
+
+/**
+ * Created by stengali on 1/21/18.
+ */
+public interface NestedInteger {
+    public boolean isInteger();
+    public Integer getInteger();
+    public List<NestedInteger> getList();
+}
